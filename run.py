bc7e9caaab3dc2a171a88396ea342f28694ade0e
Theorvolt <sai494iscool@gmail.com>
2019-11-18 17:23:15 +1100

    Removed token

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from BotRebuild import Atma
 
 
 if __name__ == '__main__':
-	atma = Atma(token='MzM4MTA0ODgzNzkyMzE0Mzc5.XLa3Jw.DrctzXdZo_DXYJM3-9dIrgd2z7U')
+	atma = Atma(token='INSERT TOKEN HERE')
 	atma.run()
